93ed121b591c90d3aa468aa278c58fdab7eb59d2
Kai Krakow <kai@kaishome.de>
2010-10-22 00:49:48 +0200

    Add German and English locale strings

--- init.rb
+++ init.rb
@@ -1,2 +1,3 @@
 require File.join(File.dirname(__FILE__), 'lib', 'modalbox_helpers')
 ActionView::Helpers::AssetTagHelper::register_javascript_include_default('modalbox')
+I18n.load_path += Dir[File.join(File.dirname(__FILE__), 'lib', 'locale', '*.{rb,yml}')]
